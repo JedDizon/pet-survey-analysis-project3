de90632d3d805e30322a4e43a855495662753908
JedDizon <ku98sovk@students.codeinstitute.net>
2025-01-06 15:35:52 +0000

    added functions to ask what animal, vaildate animal input, and to ask if more are to be inputted

--- run.py
+++ run.py
@@ -24,17 +24,18 @@ def do_you_have_pets():
         question_one = input("Does the participant have / had a pet? (Y / N)\n").strip().upper()
         print(f"User input is {question_one}\n")
         
-        if validate_do_you_have_pets(question_one):
+        if validate_y_n_question(question_one):
             if question_one == 'N':
                 print("No pets, program will end.")
                 return "No" # answered_no (Need to be defined) 
             elif question_one == "Y":
                 print("Data is valid!")
+                what_animal()
                 break
-    return question_one_answer
+    return question_one
 
 
-def validate_do_you_have_pets(answer):
+def validate_y_n_question(answer):
     """
     Validate answer given.
     Checks if answer given was Y or N.
@@ -47,16 +48,73 @@ def validate_do_you_have_pets(answer):
     except ValueError as e:
         print(f"Invalid data: {e}, please try again.\n")
         return False
-
     return True
 
-    # Validate result
-    # Ensure key pressed is Y or N - capitalize answer
-    # Else - error message and restart question
+
+def what_animal():
+    """
+    Get pet data input from user
+    """
+    while True:
+        question_two = input("What animal?\n").strip().upper()
+        print(f"User input is {question_two}\n")
+        if validate_what_animal(question_two):
+            print("Animal is valid!")
+            if not keep_asking():
+                break
+    return question_two
+
+def validate_what_animal(answer):
+    """
+    Validate answer given.
+    Checks if answer given was DOG, CAT, FISH, BIRD, OTHER.
+    """
+    try:
+        if answer not in ('DOG', 'CAT', 'FISH', 'BIRD', 'REPTILE', 'SMALL MAMMAL', 'INSECTS', 'OTHER'):
+            raise ValueError(
+                f"DOG, CAT, FISH, BIRD, REPTILE, SMALL MAMMAL, INSECTS or OTHER is required, you answered {answer}"
+                )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+    return True
+
+def keep_asking():
+    """
+    Ask user if they have another animal to input
+    """
+    while True:
+        question_three = input("Do you have another animal to add? (Y / N)\n").strip().upper()
+        print(f"User input is {question_three}\n")
+        if validate_y_n_question(question_three):
+            if question_three == 'N':
+                print("No additional animals to add")
+                return False
+            elif question_three == "Y":
+                print("More animals added")
+                what_animal()
+                return True
+
+
+def validate_another_pet(answer):
+    """
+    Validate answer given.
+    Checks if answer given was Y or N.
+    """
+    try:
+        if answer not in ('Y', 'N'):
+            raise ValueError(
+                f"Y or N is required, you answered {answer}"
+                )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+    return True
 
 
 print("Welcome to Pet Surveyor Analysis\n")
 question_one_data = do_you_have_pets()
+
 
 # Idea1
 # Welcome user to Pet Surveyor Analysis, Input function (how many have no pets) 
@@ -81,6 +139,7 @@ question_one_data = do_you_have_pets()
 # Display results as dictionary
 # Total people surveyed
 # Output to google sheet - should update graph also
+#add clear prev data option at start
 
 
 # Your code goes here.
